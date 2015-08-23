0af0c1ea50da9bd42ea9edb2aef6a9141ae61320
easyclimb <904928537@qq.com>
2015-08-23 11:02:00 +0800

    Update Practice_A.cc

--- Practice_A.cc
+++ Practice_A.cc
@@ -1,24 +1,79 @@
 #include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
 
-bool canSolve(vector<pair<string, string>>& vec) {
-    unordered_set<
+using namespace std;
+
+int color[201];
+bool visit[201];
+
+bool bfs(vector<vector<int>>& graph, int start, int n) {
+    memset(color, -1, sizeof(color));
+    memset(visit, 0, sizeof(visit));
+    queue<int> que;
+    que.push(start);
+    visit[start] = true;
+    color[start] = 0;
+    while(!que.empty()) {
+        int id = que.front();
+        que.pop();
+        int size = graph[id].size();
+        for(int i = 0; i < size; i++) {
+            int id_cur = graph[id][i];
+            if(color[id_cur] == -1) {
+                color[id_cur] = (color[id] + 1)%2;
+                visit[id_cur] = true;
+            }
+            else {
+                if(color[id_cur] == color[id]) return false;
+            }
+            que.push(id_cur);
+        }
+    }
+    for(int i = 0; i < n; i++)
+        if(!visit[i]) return bfs(graph, i, n);
+    return true;
 }
 
 int main()
 {
+    freopen("A-small-practice-1.in", "r", stdin);
+    freopen("A-small-practice-1.out", "w", stdout);
     int numCase;
     cin >> numCase;
     for(int i = 1; i <= numCase; i++) {
         int numInput;
         cin >> numInput;
-        vector<pair<string, string>> vec(numInput);
+        vector<vector<int>> graph(201);
+        unordered_map<string, int> id;
+        string a, b;
+        int cur_id = 0, id_a, id_b;
         for(int j = 0; j < numInput; j++) {
-            cin >> vec[j].first >> vec[j].second;
+            cin >> a >> b;
+            if(id.find(a) != id.end())
+                id_a = id[a];
+            else {
+                id[a] = cur_id;
+                id_a = cur_id;
+                cur_id++;
+            }
+            if(id.find(b) != id.end())
+                id_b = id[b];
+            else {
+                id[b] = cur_id;
+                id_b = cur_id;
+                cur_id++;
+            }
+            graph[id_a].push_back(id_b);
+            graph[id_b].push_back(id_a);
         }
-        if(canSolve(vec))
-            cout << "Case #%d: Yes" << endl;
+        if(bfs(graph, 0, cur_id))
+            cout << "Case #" << numCase << ": Yes" << endl;
         else
-            cout << "Case #%d: No" << endl;
+            cout << "Case #" << numCase << ": No" << endl;
     }
     return 0;
 }
